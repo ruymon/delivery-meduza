e9b496e3c97b360a175bdd3e5e54c3991cdc0098
ruymon <ruystfrancis@gmail.com>
2022-06-07 20:05:24 -0300

    :tada: Project config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand-red': '#EA1D2C',
+      },
+    },
+  },
+  plugins: [],
+} 
