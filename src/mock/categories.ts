189ee6b966a4b9311784f444d58361d8e8203843
Ruy Monteiro <ruystfrancis@gmail.com>
2022-06-08 09:11:12 -0300

    :clown_face: Generate Mock data

---
+++ src/mock/categories.ts
@@ -0,0 +1,38 @@
+export const categories = [
+    {
+        id: 1,
+        slug: "burger",
+        emoji: "🍔",
+        title: "Burger"
+    },
+    {
+        id: 2,
+        slug: "sushi",
+        emoji: "🍣",
+        title: "sushi"
+    },
+    {
+        id: 3,
+        slug: "meat",
+        emoji: "🥩",
+        title: "Churras"
+    },
+    {
+        id: 4,
+        slug: "vegetarian",
+        emoji: "🥦",
+        title: "Vegetariano"
+    },
+    {
+        id: 5,
+        slug: "deserts",
+        emoji: "🧁",
+        title: "Sobremesas"
+    },
+    {
+        id: 6,
+        slug: "pizza",
+        emoji: "🍕",
+        title: "Pizza"
+    },
+]
---
+++ src/mock/restaurants.ts
@@ -0,0 +1,20 @@
+export const restaurants = [
+    {
+        id: 1,
+        trending: true,
+        bannerImage: "",
+        name: "Black Sushi",
+        deliveryTime: "30-40",
+        budget: 3,
+        tags: [2]
+    },
+    {
+        id: 2,
+        trending: true,
+        bannerImage: "",
+        name: "The Fifties",
+        deliveryTime: "40-60",
+        budget: 3,
+        tags: [1]
+    },
+];
